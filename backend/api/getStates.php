92d25bedef675a221a42285cd06ce71aeb80b163
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-04-08 14:21:18 -0400

    update php

--- backend/api/getStates.php
+++ backend/api/getStates.php
@@ -1,10 +1,14 @@
-<?php 
-    include_once('../db/db.php');
+<?php
+include_once('../db/db.php');
 
-    $result = $conn->query("SELECT *, ROUND(land_mass) AS land_mass FROM states ORDER BY land_mass DESC");
-    $rows = array();
-    while ($row = $result->fetch_assoc()) {
-        $rows[] = $row;
-    }
-    echo json_encode($rows);
+$result = $conn->query("SELECT *, 
+    ROUND(density) AS density,
+    ROUND(land_mass) AS land_mass 
+    FROM states ORDER BY land_mass DESC
+    ");
+$rows = array();
+while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+}
+echo json_encode($rows);
 ?>

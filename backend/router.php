1ce3c6f388153a5ae9bd97f5c7531f23b042ccba
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-11-06 13:37:11 -0500

    clean up

--- backend/router.php
+++ backend/router.php
@@ -3,6 +3,6 @@
     if ($path !== '/' && file_exists(__DIR__ . $path)) {
         return false;
     }
-    if ($path === '/api/getStates') { require_once __DIR__ . '/api/getStates.php'; exit; }
-    if ($path === '/api/getProvinces') { require_once __DIR__ . '/api/getProvinces.php'; exit; }
+    if ($path === '/api/getStates') { require __DIR__ . '/api/getStates.php'; exit; }
+    if ($path === '/api/getProvinces') { require __DIR__ . '/api/getProvinces.php'; exit; }
     return false;
